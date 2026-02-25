36bace2b6f67d3f9fb6c54a34f1013efbb905e7a
J <j@TrapBook-Air.local>
2026-02-25 13:41:47 +0100

    finally i guess

--- js/classes/Feed.js
+++ js/classes/Feed.js
@@ -5,6 +5,7 @@ export default class Feed{
 		this.ALGORITHM = algo
 		this.AUDIO_CTX = audio_ctx
 
+		this._lastScrollPosition = 0
 		this.node = node,
 		this.height = 0,
 		this.posts = [],
@@ -13,52 +14,60 @@ export default class Feed{
 			ref: null
 		},
 		this.buffer ={
-			size: 5,
-			generate: 10
+			size: 10,
+			generate: 20
 		}
 
-		let f = this.events.scrollend.bind(this)
-		this.node.addEventListener("scrollend",function(e){f(e)})
-
+		let f = function(){
+			if(t){
+				clearTimeout(t)
+			}
+			this.events.scrollend.bind(this)()
+		}
+		let t = null
 
 		this.node.addEventListener("scroll",(e) => {
-			console.log(e)
-			console.log(this.node.scrollTop)
+			if(t){
+				clearTimeout(t)
+			}
+			t = setTimeout(f.bind(this), 50)
 		})
 	}
 	events = {
 		scrollend: function(e){
-			console.log("scrollend")
-			console.log(this.node.scrollTop)
-			let active = Math.floor((this.node.scrollTop + 0.5 * this.height) / this.height)
-			if(this.activePost.index != active){
-				if(this.activePost.ref){
-					this.activePost.ref.node.classList.remove("active")
-					if(this.activePost.ref.sound){
-						for(let s of this.activePost.ref.sound){
-							s.generator.disconnect()
+			if(Math.abs(this.node.scrollTop - this._lastScrollPosition) > 10){
+				this._lastScrollPosition = this.node.scrollTop
+				console.log("scrollend")
+				let active = Math.floor((this.node.scrollTop + 0.5 * this.height) / this.height)
+				if(this.activePost.index != active){
+					if(this.activePost.ref){
+						this.activePost.ref.node.classList.remove("active")
+						if(this.activePost.ref.sound){
+							for(let s of this.activePost.ref.sound){
+								s.generator.disconnect()
+							}
 						}
 					}
 				}
-			}
 
-			this.activePost.index = active
-			this.activePost.ref = this.posts[this.activePost.index]
-			this.activePost.ref.node.classList.add("active")
-			if(this.activePost.ref.sound){
-				for(let s of this.activePost.ref.sound){
-					s.generator.connect(this.AUDIO_CTX.destination)
-				}	
-				console.log(this.AUDIO_CTX.state)
-				if(this.AUDIO_CTX.state == "suspended"){
-					this.AUDIO_CTX.resume()
+				this.activePost.index = active
+				this.activePost.ref = this.posts[this.activePost.index]
+				this.activePost.ref.node.classList.add("active")
+				if(this.activePost.ref.sound){
+					for(let s of this.activePost.ref.sound){
+						s.generator.connect(this.AUDIO_CTX.destination)
+					}	
+					console.log(this.AUDIO_CTX.state)
+					if(this.AUDIO_CTX.state == "suspended"){
+						this.AUDIO_CTX.resume()
+					}
+					console.log(this.AUDIO_CTX.state)
+					console.log(this.AUDIO_CTX)
 				}
-				console.log(this.AUDIO_CTX.state)
-				console.log(this.AUDIO_CTX)
-			}
 
-			if(this.activePost.index + this.buffer.size > this.posts.length){
-				this.generate()
+				if(this.activePost.index + this.buffer.size > this.posts.length){
+					this.generate()
+				}
 			}
 		}
 	}
